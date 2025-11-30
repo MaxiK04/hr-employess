bc96d98526fe9a4ae0cc9d67d32baf9f67b888b2
MaxiK04 <masterkotlet007@gmail.com>
2025-11-30 17:57:22 +0300

    connect Database postgre

--- backend.js
+++ backend.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const app = express();
 const PORT = 3000;
@@ -7,9 +9,26 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+// Подключение к БД
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'employess_db',
+  password: 'qwertyui',
+  port: 5432,
+});
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Сервер работает! 🚀', timestamp: new Date() });
+// Проверка подключенияБД
+app.get('/api/test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW() as time');
+    res.json({ 
+      message: 'Сервер и БД работают', 
+      dbTime: result.rows[0].time 
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Ошибка БД: ' + error.message });
+  }
 });
 
 app.listen(PORT, () => {
